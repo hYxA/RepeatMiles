682d418295bdd20d926d630dcbda059e69f5c4e5
hYxA <zybu@live.ru>
2021-01-21 09:19:47 +0300

    calculate miles

---
+++ src/main/java/Miles.java
@@ -0,0 +1,9 @@
+public class Miles {
+
+    public static void MilesCalculate(String[] args) {
+        int countTicket = 12700;
+        int bonusMiles = countTicket / 20;
+        System.out.println("Bonus = " + bonusMiles);
+    }
+
+}
